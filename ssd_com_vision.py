c91e82302121b9ccd7c7f285c3fe7257a7503998
it63070237 <73685051+it63070237@users.noreply.github.com>
2021-02-16 02:53:14 +0700

    Update ssd_com_vision.py

--- ssd_com_vision.py
+++ ssd_com_vision.py
@@ -14,15 +14,15 @@ blue = blue[100:1000, 100:1050]
 green = green[100:1000, 100:1050]
 
 down_shape_red = red.copy()
-for i in range(3):
+for i in range(2):
     down_shape_red = cv2.pyrDown(down_shape_red)
 
 down_shape_green = green.copy()
-for i in range(3):
+for i in range(2):
     down_shape_green = cv2.pyrDown(down_shape_green)
 
 down_shape_blue = blue.copy()
-for i in range(3):
+for i in range(2):
     down_shape_blue = cv2.pyrDown(down_shape_blue)
 
 sz_test = down_shape_red.shape
@@ -34,25 +34,32 @@ im_color_down = np.zeros((height, width, 3), dtype=np.uint8)
 im_color_down[:, :, 2] = down_shape_red[:, :, 0]
 
 u, v, ssd_now = 0, 0, 10
-for hei in range(1, height):
-    for wid in range(1, width):
+for hei in range(2, height-1):
+    for wid in range(2, width-1):
         test_window = im_color_down.copy()
         ali = down_shape_green.copy()
         test_window[:-hei, :-wid, 1] = ali[hei:, wid:, 0]
-        if ssd(test_window[:, :, 2], test_window[:, :, 1]) < ssd_now:
-            ssd_now = ssd(test_window[:, :, 2], test_window[:, :, 1])
-            u = hei*8
-            v = wid*8
+        if ssd(test_window[:-hei, :-wid, 2], test_window[:-hei, :-wid, 1]) < ssd_now:
+            ssd_now = ssd(test_window[:-hei, :-wid, 2], test_window[:-hei, :-wid, 1])
+            u = hei
+            v = wid
 
+im_color_down[:-u, :-v, 1] = down_shape_green[u:, v:, 0]
+print(u, v)
 
-sz = red.shape
-height_1 = int(sz[0])
-width_1 = sz[1]
-im_color = np.zeros((height_1, width_1, 3), dtype=np.uint8)
-im_color[:, :, 2] = red[:, :, 0]
-im_color[:-u, :-v, 1] = green[u:, v:, 0]
-print(u,v)
+u, v, ssd_now = 0, 0, 10
+for hei in range(2, height-1):
+    for wid in range(2, width-1):
+        test_window = im_color_down.copy()
+        ali = down_shape_blue.copy()
+        test_window[:-hei, :-wid, 1] = ali[hei:, wid:, 0]
+        if ssd(test_window[:-hei, :-wid, 2], test_window[:-hei, :-wid, 1]) < ssd_now:
+            ssd_now = ssd(test_window[:-hei, :-wid, 2], test_window[:-hei, :-wid, 1])
+            u = hei
+            v = wid
 
+im_color_down[:-u, :-v, 0] = down_shape_blue[u:, v:, 0]
+print(u, v)
 
-cv2.imshow('test', im_color)
+cv2.imshow('test', im_color_down)
 cv2.waitKey(0)
