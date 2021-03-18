addfb701dd6f00a98494c07eff14621a5a8f9ad5
it63070237 <73685051+it63070237@users.noreply.github.com>
2021-03-18 16:50:12 +0700

    Update ssd_com_vision.py

--- ssd_com_vision.py
+++ ssd_com_vision.py
@@ -21,7 +21,7 @@ print(green.shape)
 height = int(sz_test[0])
 width = sz_test[1]
 
-im_color = np.zeros((100, 100, 3), dtype=np.uint8)
+im_color = np.zeros((100, 100, 3))
 im_color[:, :, 2] = red[(height//2)-50:(height//2)+50, (width//2)-50:(width//2)+50, 0]
 im_color_slide = np.zeros((height, width, 3), dtype=np.uint8)
 im_color_slide[:, :, 2] = red[:, :, 0]
@@ -42,7 +42,7 @@ print('ans1 : ',u ,v)
 if u <= 0 and v <= 0:
     im_color_slide[:height-abs(u), :width-abs(v), 1] = green[abs(u):, abs(v):, 0]
 elif u <= 0 and v > 0:
-    im_color_slide[:height - abs(u), v:, 1] = green[abs(u):, :width-abs(v), 0]
+    im_color_slide[:abs(u), :-v, 1] = green[:u, v:, 0]
 elif u > 0 and v <= 0:
     im_color_slide[u:, :width - abs(v), 1] = green[:height-abs(u), abs(v):, 0]
 elif u > 0 and v > 0:
@@ -50,8 +50,8 @@ elif u > 0 and v > 0:
 
 
 u, v, ssd_now = 0, 0, 10
-for hei in range(-15, 16, 1):
-    for wid in range(-15, 16, 1):
+for hei in range(-30, 31, 1):
+    for wid in range(-30, 31, 1):
         test_window = im_color.copy()
         test_window[:, :, 0] = blue[(height//2)-50+hei:(height//2)+50+hei, (width//2)-50+wid:(width//2)+50+wid, 0]
         if ssd(test_window[:, :, 2], test_window[:, :, 0]) < ssd_now:
@@ -61,6 +61,8 @@ for hei in range(-15, 16, 1):
 
 cv2.imshow('test2', test_window)
 print(ssd_now)
+v *= -1
+u *= -1
 print('ans2 : ',u ,v)
 if u <= 0 and v <= 0:
     im_color_slide[abs(u):, abs(v):, 0] = blue[:u, :v, 0]
@@ -73,4 +75,3 @@ elif u > 0 and v > 0:
 
 cv2.imshow('real', im_color_slide)
 cv2.waitKey(0)
-
