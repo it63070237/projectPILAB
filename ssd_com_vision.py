7bd095c20aa81b4353449d88730cb09724aebd1e
it63070237 <73685051+it63070237@users.noreply.github.com>
2021-03-18 14:14:37 +0700

    Update ssd_com_vision.py

--- ssd_com_vision.py
+++ ssd_com_vision.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 def ssd(img2, img1):
-    return float(np.sqrt(float(np.sum((np.array(img1).flatten()-np.array(img2).flatten())**2))))/(img1.shape[0]*img1.shape[1])
+    return float(np.sqrt(float(np.sum((np.array(img1).flatten()-np.array(img2).flatten())**2))))/(img2.shape[0]*img2.shape[1])
 
 
 red = cv2.imread('red.jpg')
@@ -27,8 +27,8 @@ im_color_slide = np.zeros((height, width, 3), dtype=np.uint8)
 im_color_slide[:, :, 2] = red[:, :, 0]
 
 u, v, ssd_now = 0, 0, 10
-for hei in range(-15, 15, 1):
-    for wid in range(-15, 15, 1):
+for hei in range(-15, 16, 1):
+    for wid in range(-15, 16, 1):
         test_window = im_color.copy()
         test_window[:, :, 1] = green[(height//2)-50+hei:(height//2)+50+hei, (width//2)-50+wid:(width//2)+50+wid, 0]
         if ssd(test_window[:, :, 2], test_window[:, :, 1]) < ssd_now:
@@ -50,8 +50,8 @@ elif u > 0 and v > 0:
 
 
 u, v, ssd_now = 0, 0, 10
-for hei in range(-15, 15, 1):
-    for wid in range(-15, 15, 1):
+for hei in range(-15, 16, 1):
+    for wid in range(-15, 16, 1):
         test_window = im_color.copy()
         test_window[:, :, 0] = blue[(height//2)-50+hei:(height//2)+50+hei, (width//2)-50+wid:(width//2)+50+wid, 0]
         if ssd(test_window[:, :, 2], test_window[:, :, 0]) < ssd_now:
@@ -73,3 +73,4 @@ elif u > 0 and v > 0:
 
 cv2.imshow('real', im_color_slide)
 cv2.waitKey(0)
+
